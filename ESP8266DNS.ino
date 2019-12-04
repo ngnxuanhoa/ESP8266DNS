08a7bbe23d7e778d7d89c3ca345f825470ee5557
ngnxuanhoa <xuan.n.hoa@gmail.com>
2019-12-04 20:04:02 +0700

    Update ESP8266DNS.ino

--- ESP8266DNS.ino
+++ ESP8266DNS.ino
@@ -31,19 +31,13 @@ void handleRoot() {
       
       cmd += "<head>";
       cmd += "<meta http-equiv='refresh' content='5'/>";
-      //cmd +=("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-      //cmd +=(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-      //cmd +=("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-      //cmd +=(".button2 {background-color: #77878A;}</style>");
       cmd += "</head>";
       
       if(device1){
         cmd +=("<br/>Device0ON");
-        //cmd +=("<p><button class=\"button\">ON</button></p>");
       }
       else{
         cmd +=("<br/>Device0OFF");
-        //cmd +=("<p><button class=\"button button2\">OFF</button></p>");
       }
       
       if(device2){
@@ -94,10 +88,10 @@ void setup(void){
   pinMode(output3, OUTPUT);
   pinMode(output4, OUTPUT);
 
-  digitalWrite(output1, LOW); 
-  digitalWrite(output2, LOW);
-  digitalWrite(output3, LOW);
-  digitalWrite(output4, LOW);
+  digitalWrite(output1, HIGH); 
+  digitalWrite(output2, HIGH);
+  digitalWrite(output3, HIGH);
+  digitalWrite(output4, HIGH);
    
   Serial.begin(115200);
   
@@ -119,55 +113,55 @@ void setup(void){
   server.on("/socket0On", [](){
     server.send(200, "text/plain", "device1 = ON");
     Serial.println("device1 = ON");
-    digitalWrite(output1, HIGH); 
+    digitalWrite(output1, LOW); 
     device1 = true;
   });
 
   server.on("/socket0Off", [](){
     server.send(200, "text/plain", "device1 = OFF");
     Serial.println("device1 = OFF");
-    digitalWrite(output1, LOW); 
+    digitalWrite(output1, HIGH); 
     device1 = false;
   });
 
   server.on("/socket1On", [](){
     server.send(200, "text/plain", "device2 = ON");
-    digitalWrite(output2, HIGH); 
+    digitalWrite(output2, LOW); 
     Serial.println("device2 = ON");
     device2 = true;
   });
 
   server.on("/socket1Off", [](){
     server.send(200, "text/plain", "device2 = OFF");
-    digitalWrite(output2, LOW); 
+    digitalWrite(output2, HIGH); 
     Serial.println("device2 = OFF");
     device2 = false;
   });
 
   server.on("/socket2On", [](){
     server.send(200, "text/plain", "device3 = ON");
-    digitalWrite(output3, HIGH); 
+    digitalWrite(output3, LOW); 
     Serial.println("device3 = ON");
     device3 = true;
   });
 
   server.on("/socket2Off", [](){
     server.send(200, "text/plain", "device3 = OFF");
-    digitalWrite(output3, LOW); 
+    digitalWrite(output3, HIGH); 
     Serial.println("device3 = OFF");
     device3 = false;
   });
 
   server.on("/socket3On", [](){
     server.send(200, "text/plain", "device4 = ON");
-    digitalWrite(output4, HIGH); 
+    digitalWrite(output4, LOW); 
     Serial.println("device4 = ON");
     device4 = true;
   });
 
   server.on("/socket3Off", [](){
     server.send(200, "text/plain", "device4 = OFF");
-    digitalWrite(output4, LOW);
+    digitalWrite(output4, HIGH);
     Serial.println("device4 = OFF"); 
     device4 = false;
   });
